28d67606bc8d6399be059eefb1dd30e2d6b3dcf2
Kevin Ng <163484406+kevincngg@users.noreply.github.com>
2026-05-20 11:31:03 -0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 25
+float_type = 98.6
+bool_type = False
+string_type = "kevi"
+list_type = [6,7]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
