78f2ad92e2f69920335e6ceb2defad1295e7494f
Kevin Ng <163484406+kevincngg@users.noreply.github.com>
2026-06-19 08:02:08 -0500

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 20.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "s"
+print(type(variable))
+variable = [2,1]
+print(type(variable))
