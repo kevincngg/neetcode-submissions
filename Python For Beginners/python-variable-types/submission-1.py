c049db1ab63c2f70bba7db7f05cb4a9cda2ac3be
Kevin Ng <163484406+kevincngg@users.noreply.github.com>
2026-06-19 08:01:17 -0500

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,14 @@
+integer_type = 10
+float_type = 9.1
+bool_type = True
+string_type = "K"
+list_type = [1,2]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
