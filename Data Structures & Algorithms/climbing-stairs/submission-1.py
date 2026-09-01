2ba47d93ea21261ae1e3a1aaea0c1be695b50e9c
Kevin Ng <163484406+kevincngg@users.noreply.github.com>
2026-09-01 14:28:23 -0500

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def __init__(self):
+        self.memo = {1: 1, 2: 2}
+    def climbStairs(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
+
+
+        
